b071df54e22d8c1a7dec3ddaef7494b4625ebdce
Will Penington <will@willpenington.com>
2015-10-15 20:35:38 +1100

    Add Basic Message Recieve implementation
    Single shot message recieve implementation added

--- MailboxQt/msglistener.cpp
+++ MailboxQt/msglistener.cpp
@@ -13,7 +13,18 @@ MsgListener::MsgListener(int fd, QObject *parent) :
 
 void MsgListener::run()
 {
+    int length;
+    erlang_msg msg;
+    ei_x_buff buff;
 
+    ei_x_new(&buff);
+
+    length = ei_xreceive_msg_tmo(m_fd, &msg, &buff, 1000);
+
+    if (length > 0 && (msg.msgtype == ERL_SEND || msg.msgtype == ERL_REG_SEND))
+        emit messageRecieved();
+
+    ei_x_free(&buff);
 }
 
 } // namespace Mailbox
